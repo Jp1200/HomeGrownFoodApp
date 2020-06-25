684be0e1c39e708e22f87ed872df77485dacd218
Jp1200 <jprkr96@gmail.com>
2020-06-25 18:51:01 -0500

    second commit

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text>Hello</Text>
+        <Text>Welcome to the Future of Gardening</Text>
+        <Button style={styles.button} title="Login"></Button>
+        <Button title="Sign Up"></Button>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: "auto",
+    includeFontPadding: true,
+  },
+  button: {
+    color: "#ffffff",
   },
 });
